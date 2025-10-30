be0a0937080e1f1c22477a745ebc960f2f7a1570
mtpezedev <mtpezequiel@gmail.com>
2025-10-30 14:26:29 +0100

    Hero de gallo y fix de home y titulo

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import Navbar from "./Navbar.jsx";
+import HeroRooster from "./Hero.jsx";
 
 //create your first component
 const Home = () => {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      <HeroRooster />
+    </>
+  );
 };
 
 export default Home;
