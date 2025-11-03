8f277702db437c8f13f809ae20272e9a5a522baa
gustavomesa <gustavoamesa17@gmail.com>
2025-11-03 13:54:07 +0100

    feet: seccion de cartas Gustavo Mesa

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,25 @@
 import React from "react";
+import Navbar from "./Navbar.jsx";
+import Hero from "./Hero.jsx";
+import Card from "./Card.jsx";
+import { rosterData } from "../../data/rosterData.js";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
-
-export default Home;
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <div className="container py-4 d-flex flex-wrap gap-4 justify-content-center">
+        {rosterData.map((item, i) => (
+          <Card
+            key={i}
+            image={item.image}
+            cardTitle={item.cardTitle}
+            cardDescription={item.cardDescription}
+            button={item.button}
+          />
+        ))}
+      </div>
+    </>
+  );
+}
